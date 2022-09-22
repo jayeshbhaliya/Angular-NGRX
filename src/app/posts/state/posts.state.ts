f1b9a5d40c9985a266c3939f069c6baf7f2ec914
jayeshbhaliya <bhaliyajayesh4951@gmail.com>
2022-09-22 17:26:37 +0530

    ngrx/entity

--- src/app/posts/state/posts.state.ts
+++ src/app/posts/state/posts.state.ts
@@ -4,6 +4,12 @@ import { Post } from "src/app/model/posts.model"
 
 export interface PostsState  extends EntityState<Post>{}
 
-export const postsAdapter = createEntityAdapter<Post>();
+export const postsAdapter = createEntityAdapter<Post>({
+  sortComparer: sortByName
+});
 
 export const initialState : PostsState = postsAdapter.getInitialState();
+
+export function sortByName(a: Post, b: Post): number {
+  return a.title.localeCompare(b.title);
+}
